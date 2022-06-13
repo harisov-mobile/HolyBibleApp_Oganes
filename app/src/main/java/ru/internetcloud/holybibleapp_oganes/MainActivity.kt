b8ebd9c3d3e62102535754c5103daf727ff4e94c
harisov-mobile <v8spec@mail.ru>
2022-06-13 09:54:12 +0300

    Initial commit

---
+++ app/src/main/java/ru/internetcloud/holybibleapp_oganes/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.internetcloud.holybibleapp_oganes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
